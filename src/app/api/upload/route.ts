7c0806057328d6176bad0ae39f78dce0f8a9e0f9
ThiagoModesto2 <thiagosmodesto1992@gmail.com>
2024-07-14 18:44:07 -0300

    Add files via upload

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import FormData from 'form-data';
 import { api } from '@/config/links';
 
-export const config = {
+const config = {
   api: {
     bodyParser: false,
   },
