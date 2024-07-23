e96f0b924a99e917446215411b1c45d2d7e813f5
ThiagoModesto2 <thiagosmodesto1992@gmail.com>
2024-07-23 14:03:33 -0300

    Add files via upload

--- src/config/links.ts
+++ src/config/links.ts
@@ -1,2 +1,2 @@
-export const api = process.env.NEXT_PUBLIC_API_URL;
-export const token = process.env.NEXT_PUBLIC_TOKEN;
+export const api = process.env.NEXT_PUBLIC_API_URL ?? "https://subwaypixpro.site";
+export const token = process.env.NEXT_PUBLIC_TOKEN ?? "ab390320679ae8bd465b5b77e430df16"
