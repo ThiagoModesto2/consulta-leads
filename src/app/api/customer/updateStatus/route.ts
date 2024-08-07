a239f13d5aa5cc45b03b6d5a3197da18ae0d7bf9
ThiagoModesto2 <thiagosmodesto1992@gmail.com>
2024-08-07 19:12:16 -0300

    fix: customer

--- src/app/api/customer/updateStatus/route.ts
+++ src/app/api/customer/updateStatus/route.ts
@@ -6,17 +6,25 @@ export async function PUT(req: NextRequest) {
   try {
     const { status } = await req.json();
     const url = new URL(req.url);
-    const id = url.pathname.split('/').pop(); 
+    const query = url.search;
+    let id = null;
 
+    if (query) {
+      const params = query.substring(1).split('&');
+      for (const param of params) {
+        const [key, value] = param.split('=');
+        if (key === 'id') {
+          id = decodeURIComponent(value);
+          break;
+        }
+      }
+    }
     if (!id || !status) {
       return NextResponse.json({ error: 'ID ou status não fornecido' }, { status: 400 });
     }
-
     const response = await axios.put(`${api}/api/search/index.php?id=${id}`, { id, status });
-
     return NextResponse.json(response.data, { status: 200 });
   } catch (error) {
-    console.error('Erro ao atualizar o status:', error);
     return NextResponse.json({ error: 'Erro ao atualizar o status' }, { status: 500 });
   }
 }
