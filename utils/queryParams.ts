c366dc5f8f18d5de29e2cdaff0ca189502940b10
ThiagoModesto2 <thiagosmodesto1992@gmail.com>
2024-08-07 19:12:52 -0300

    Delete utils directory

--- utils/queryParams.ts
+++
@@ -1,6 +0,0 @@
-export const queryParams = (link: string) => {
-    const currentParams = new URLSearchParams(window.location.search);
-    const url = `${link}?${currentParams.toString()}`;
-
-    return url;
-}
